7fef93a06d05a9f5e7cd6be361a61d22c43494ac
najiMousa <ana.naji.1996@gmail.com>
2025-05-13 22:28:40 +0300

    01
    Fixed CV section

--- lib/pages/home/components/cv_section.dart
+++ lib/pages/home/components/cv_section.dart
@@ -10,25 +10,25 @@ final List<DesignProcess> designProcesses = [
     title: "DESIGN",
     imagePath: "assets/design.png",
     subtitle:
-        "A full stack allround designer thay may or may not include a guide for specific creative",
+    "A full stack allround designer thay may or may not include a guide for specific creative",
   ),
   DesignProcess(
     title: "DEVELOP",
     imagePath: "assets/develop.png",
     subtitle:
-        "A full stack allround developer thay may or may not include a guide for specific creative",
+    "A full stack allround developer thay may or may not include a guide for specific creative",
   ),
   DesignProcess(
     title: "WRITE",
     imagePath: "assets/write.png",
     subtitle:
-        "A full stack allround writer thay may or may not include a guide for specific creative",
+    "A full stack allround writer thay may or may not include a guide for specific creative",
   ),
   DesignProcess(
     title: "PROMOTE",
     imagePath: "assets/promote.png",
     subtitle:
-        "A full stack allround promoter thay may or may not include a guide for specific creative",
+    "A full stack allround promoter thay may or may not include a guide for specific creative",
   ),
 ];
 
@@ -48,111 +48,113 @@ class CvSection extends StatelessWidget {
   Widget _buildUi(BuildContext context, double width) {
     // we need the context to get maxWidth before the constraints below
     return Container(
-      constraints: BoxConstraints(
-        maxWidth: width,
-        minWidth: width,
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "BETTER DESIGN,\nBETTER EXPERIENCES",
-                style: GoogleFonts.oswald(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w900,
-                  height: 1.8,
-                  fontSize: 18.0,
+      alignment: Alignment.center,
+      child: Container(
+        constraints: BoxConstraints(
+          maxWidth: width,
+          minWidth: width,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "BETTER DESIGN,\nBETTER EXPERIENCES",
+                  style: GoogleFonts.oswald(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w900,
+                    height: 1.8,
+                    fontSize: 18.0,
+                  ),
                 ),
-              ),
-              GestureDetector(
-                onTap: () {},
-                child: MouseRegion(
-                  cursor: SystemMouseCursors.click,
-                  child: Text(
-                    "DOWNLOAD CV",
-                    style: GoogleFonts.oswald(
-                      color: kPrimaryColor,
-                      fontWeight: FontWeight.w900,
-                      fontSize: 16.0,
+                GestureDetector(
+                  onTap: () {},
+                  child: MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: Text(
+                      "DOWNLOAD CV",
+                      style: GoogleFonts.oswald(
+                        color: kPrimaryColor,
+                        fontWeight: FontWeight.w900,
+                        fontSize: 16.0,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 50.0,
-          ),
-          Container(
-            child: LayoutBuilder(
-              builder: (_context, constraints) {
-                return ResponsiveGridView.builder(
-                  padding: EdgeInsets.all(0.0),
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  alignment: Alignment.topCenter,
-                  gridDelegate: ResponsiveGridDelegate(
-                    mainAxisSpacing: 20.0,
-                    crossAxisSpacing: 20.0,
-                    maxCrossAxisExtent: ScreenHelper.isTablet(context) ||
-                            ScreenHelper.isMobile(context)
-                        ? constraints.maxWidth / 2.0
-                        : 250.0,
-                    // Hack to adjust child height
-                    childAspectRatio: ScreenHelper.isDesktop(context)
-                        ? 1
-                        : MediaQuery.of(context).size.aspectRatio * 1.5,
-                  ),
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Image.asset(
-                                designProcesses[index].imagePath!,
-                                width: 40.0,
-                              ),
-                              SizedBox(
-                                width: 15.0,
-                              ),
-                              Text(
-                                designProcesses[index].title!,
-                                style: GoogleFonts.oswald(
-                                  fontSize: 20.0,
-                                  fontWeight: FontWeight.w700,
-                                  color: Colors.white,
-                                ),
-                              )
-                            ],
-                          ),
-                          SizedBox(
-                            height: 15.0,
-                          ),
-                          Text(
-                            designProcesses[index].subtitle!,
-                            style: TextStyle(
-                              color: kCaptionColor,
-                              height: 1.5,
-                              fontSize: 14.0,
-                            ),
-                          )
-                        ],
-                      ),
-                    );
-                  },
-                  itemCount: designProcesses.length,
-                );
-              },
+              ],
             ),
-          )
-        ],
+            SizedBox(
+              height: 50.0,
+            ),
+            Container(
+              child: LayoutBuilder(
+                builder: (_context, constraints) {
+                  return ResponsiveGridView.builder(
+                    padding: EdgeInsets.all(0.0),
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    alignment: Alignment.topCenter,
+                    gridDelegate: ResponsiveGridDelegate(
+                      mainAxisSpacing: 20.0,
+                      crossAxisSpacing: 20.0,
+                      maxCrossAxisExtent: ScreenHelper.isTablet(context) ||
+                          ScreenHelper.isMobile(context)
+                          ? constraints.maxWidth / 2.0
+                          : 250.0,
+                      // Hack to adjust child height
+                      childAspectRatio: ScreenHelper.isDesktop(context)
+                          ? 1
+                          : MediaQuery.of(context).size.aspectRatio * 1.5,
+                    ),
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Image.asset(
+                                  designProcesses[index].imagePath!,
+                                  width: 40.0,
+                                ),
+                                SizedBox(
+                                  width: 15.0,
+                                ),
+                                Text(
+                                  designProcesses[index].title!,
+                                  style: GoogleFonts.oswald(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w700,
+                                    color: Colors.white,
+                                  ),
+                                )
+                              ],
+                            ),
+                            SizedBox(
+                              height: 15.0,
+                            ),
+                            Text(
+                              designProcesses[index].subtitle!,
+                              style: TextStyle(
+                                color: kCaptionColor,
+                                height: 1.5,
+                                fontSize: 14.0,
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                    itemCount: designProcesses.length,
+                  );
+                },
+              ),
+            )
+          ],
+        ),
       ),
-    );
-  }
+    );}
 }
